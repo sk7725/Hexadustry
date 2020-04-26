f904346c7530ebc68c54109ba2f969d68206b712
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-26 23:51:06 +0900

    Update copperwall.js

--- scripts/copperwall.js
+++ scripts/copperwall.js
@@ -1,8 +1,21 @@
-
+const shadowcolor=new Color(0,0,0,0.71);
 const copperwall = extendContent(Wall, "copperwall", {
   draw(tile) {
+    /*
+    Draw.proj().setOrtho(0, 0, Vars.tilesize, Vars.tilesize);
     Fill.rect(tile.drawx()+(Math.floor(tile.y)%2)*Vars.tilesize/2+0.5, tile.drawy()+0.5, 1, 1);
+    */
+
+    //Draw.rect("shadow-hex"), tile.drawx()+(Math.floor(tile.y)%2)*Vars.tilesize/2, tile.drawy());
     Draw.rect(Core.atlas.find(this.name), tile.drawx()+(Math.floor(tile.y)%2)*Vars.tilesize/2, tile.drawy());
+  },
+  drawPlace(x,y,rotation,valid) {
+    if(valid){
+      Draw.rect(Core.atlas.find(this.name), x+(Math.floor(y)%2)*Vars.tilesize/2, y);
+    }
+    else{
+      this.super$drawPlace(x,y,rotation,valid);
+    }
   }
   /*
   bounds(x,y,rect){
