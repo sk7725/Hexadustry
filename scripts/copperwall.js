fad6fa1bf7518061d47a9e974cfcafd29e240b72
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-26 22:25:48 +0900

    hex

--- scripts/copperwall.js
+++ scripts/copperwall.js
@@ -1,6 +1,6 @@
 
 const copperwall = extendContent(Block, "copperwall", {
   draw(tile) {
-    Draw.rect(Core.atlas.find(this.name), tile.drawx()+(Math.floor(tile.x)%2)*Vars.tilesize/2, tile.drawy());
+    Draw.rect(Core.atlas.find(this.name), tile.drawx()+(Math.floor(tile.y)%2)*Vars.tilesize/2, tile.drawy());
   }
 });
