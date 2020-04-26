60e2f36a86edc0b29193e4b5d31d9f6ff04c4b74
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-26 22:42:41 +0900

    Update copperwall.js

--- scripts/copperwall.js
+++ scripts/copperwall.js
@@ -4,6 +4,12 @@ const copperwall = extendContent(Wall, "copperwall", {
     Draw.rect(Core.atlas.find(this.name), tile.drawx()+(Math.floor(tile.y)%2)*Vars.tilesize/2, tile.drawy());
   },
   bounds(x,y,rect){
-    return rect.setSize(this.size * Vars.tilesize).setCenter(x * Vars.tilesize +(Math.floor(y)%2)*Vars.tilesize/2+ offset(), y * Vars.tilesize + offset());
+    try{
+      return rect.setSize(this.size * Vars.tilesize).setCenter(x * Vars.tilesize +(Math.floor(y)%2)*Vars.tilesize/2+ this.offset(), y * Vars.tilesize + this.offset());
+    }
+    catch(err){
+      print("HEX: "+err);
+      this.super$bounds(x,y,rect);
+    }
   }
 });
