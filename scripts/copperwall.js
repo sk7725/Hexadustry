6495abb9270cfa6dcac9a998e97047008dd3f967
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-27 12:46:36 +0900

    Update copperwall.js

--- scripts/copperwall.js
+++ scripts/copperwall.js
@@ -22,6 +22,10 @@ const copperwall = extendContent(Wall, "copperwall", {
       print("E: "+err);
     }*/
   },
+  bounds(x, y, rect){
+    var offset=Math.floor(y/Vars.tilesize)*Vars.tilesize/2;
+    return rect.setSize(this.size * Vars.tilesize).setCenter(x * Vars.tilesize + offset, y * Vars.tilesize);
+  },
   drawRequestRegion(req, list){
     print("req start");
     var reg = this.getRequestRegion(req, list);
