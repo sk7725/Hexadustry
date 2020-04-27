cb6f215307afd7ea59491be3f379063276908ee1
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-27 12:07:54 +0900

    Update copperwall.js

--- scripts/copperwall.js
+++ scripts/copperwall.js
@@ -2,5 +2,21 @@ const shadowcolor=new Color(0,0,0,0.71);
 const copperwall = extendContent(Wall, "copperwall", {
   draw(tile) {
     Draw.rect(Core.atlas.find(this.name), tile.drawx()+(Math.floor(tile.y)%2)*Vars.tilesize/2, tile.drawy());
+  }.
+  getRequestRegion(req, list){
+    try{
+      var geticon=this.icon(Cicon.full);
+      var y=Math.floor(geticon.getY()/Vars.tilesize);
+      print("Y: "+y);
+      if(y%2==1){
+        return geticon.scroll(0.5,0);
+      }
+      else{
+        return geticon;
+      }
+    }
+    catch(err){
+      print(err);
+    }
   }
 });
